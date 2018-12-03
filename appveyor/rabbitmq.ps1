b2f568df0da76ff7f455c19696180f2c664a124b
bmjjr <bob@smewp.net>
2018-12-03 17:58:31 +0800

    revert back to 3.7.5 for troubleshooting appveyor

--- appveyor/rabbitmq.ps1
+++ appveyor/rabbitmq.ps1
@@ -15,13 +15,13 @@
 Write-Host "Installing RabbitMQ..." -ForegroundColor Cyan
 
 Write-Host "Downloading..."
-$exePath = "$($env:USERPROFILE)\rabbitmq-server-3.7.9.exe"
-(New-Object Net.WebClient).DownloadFile('https://dl.bintray.com/rabbitmq/all/rabbitmq-server/3.7.9/rabbitmq-server-3.7.9.exe', $exePath)
+$exePath = "$($env:USERPROFILE)\rabbitmq-server-3.7.5.exe"
+(New-Object Net.WebClient).DownloadFile('https://dl.bintray.com/rabbitmq/all/rabbitmq-server/3.7.5/rabbitmq-server-3.7.5.exe', $exePath)
 
 Write-Host "Installing..."
 cmd /c start /wait $exePath /S
 
-$rabbitPath = 'C:\Program Files\RabbitMQ Server\rabbitmq_server-3.7.9'
+$rabbitPath = 'C:\Program Files\RabbitMQ Server\rabbitmq_server-3.7.5'
 
 Write-Host "Installing service..."
 Start-Process -Wait "$rabbitPath\sbin\rabbitmq-service.bat" "install"
