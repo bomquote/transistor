2797ed91a225824c19a34d675a8da77d514e6a31
bmjjr <bob@smewp.net>
2019-09-18 20:19:50 +0800

    troubleshoot appveyor erlang

--- appveyor/install_erlang.ps1
+++ appveyor/install_erlang.ps1
@@ -1,5 +1,10 @@
-Write-Host "Installing Erlang..." -ForegroundColor Cyan
+Write-Host "Uninstalling Erlang 9.2..." -ForegroundColor Cyan
+Write-Host "Uninstalling..."
+Remove-Path 'C:\Program Files\erl9.2\bin'
+$uninstallErlang = "C:\Program Files\erl9.2\Uninstall.exe"
+cmd /c start /wait $uninstallErlang /S
 
+Write-Host "Installing Erlang 10.5..." -ForegroundColor Cyan
 Write-Host "Downloading..."
 $exePath = "$($env:USERPROFILE)\otp_win64_22.1.exe"
 (New-Object Net.WebClient).DownloadFile('http://www.erlang.org/download/otp_win64_22.1.exe', $exePath)
@@ -12,10 +17,9 @@ Remove-Path 'C:\Program Files\erl7.1\bin'
 Remove-Path 'C:\Program Files\erl7.3\bin'
 Remove-Path 'C:\Program Files\erl8.2\bin'
 Remove-Path 'C:\Program Files\erl8.3\bin'
-Remove-Path 'C:\Program Files\erl9.2\bin'
 
-Add-Path 'C:\Program Files\erl10.4\bin'
-[Environment]::SetEnvironmentVariable('ERLANG_HOME', 'C:\Program Files\erl10.4', 'Machine')
+Add-Path 'C:\Program Files\erl10.5\bin'
+[Environment]::SetEnvironmentVariable('ERLANG_HOME', 'C:\Program Files\erl10.5', 'Machine')
 
 # C:\Program Files\erl10.4
 
@@ -26,4 +30,4 @@ $x64items + @(Get-ChildItem "HKLM:SOFTWARE\wow6432node\Microsoft\Windows\Current
     | Sort-Object -Property DisplayName `
     | Select-Object -Property DisplayName,DisplayVersion
 
-Write-Host "Installed Erlang" -ForegroundColor Green
+Write-Host "Installed Erlang 10.5" -ForegroundColor Green
